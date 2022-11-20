46b1b3a846b468a46c931d50c7a8d84c6abb8719
Sicantra <igorkiril29@gmail.com>
2022-11-20 17:14:16 +0100

    add todo and todoList

--- main.go
+++ main.go
@@ -11,3 +11,18 @@ func main() {
 	// run the Gin server
 	router.Run()
 }
+
+type todo struct {
+	ID   string `json:"id"`
+	Task string `json:"task"`
+}
+
+type message struct {
+	Message string `json:"message"`
+}
+
+var todoList = []todo{
+	{"1", "Learn Go"},
+	{"2", "Build an API with Go"},
+	{"3", "Document the API with swag"},
+}
